5605743b1a78de3852981e40603d1e5e3937b4dd
s1f10210254 <s1f10210248@iniad.org>
2023-09-16 17:12:31 +0900

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,68 @@
-import styles from './index.module.css';
+import { useState } from 'react';
 
 const Home = () => {
+  interface Props {
+    name: string;
+    onButtonClick: (name: string) => void;
+  }
+
+  interface ItemData {
+    name: string;
+    imagePath: string;
+
+    description: string;
+  }
+
+  const itemData: Record<string, ItemData> = {
+    LED: {
+      name: 'LED',
+      imagePath: 'path_to_your_image/image1.jpeg',
+      description: '項目1の説明箇所です',
+    },
+    SERVO: {
+      name: '項目2',
+      imagePath: 'path_to_your_image/image2.jpeg',
+      description: '項目2の説明箇所です',
+    },
+    項目3: {
+      name: '項目3',
+      imagePath: 'path_to_your_image/image3.jpeg',
+      description: '項目3の説明箇所です',
+    },
+  };
+
+  const CheckboxItem: React.FC<Props> = ({ name, onButtonClick }) => {
+    const [isChecked, setIschecked] = useState(false);
+    const [count, setCount] = useState(0);
+
+    return (
+      <div>
+        <input type="checkbox" checked={isChecked} onChange={() => setIschecked(!isChecked)} />
+        {name}
+        <input type="number" value={count} onChange={(e) => setCount(Number(e.target.value))} />
+        <button onClick={() => onButtonClick(name)}>ボタン</button>
+      </div>
+    );
+  };
+
+  const [selectedItem, setSelectedItem] = useState<ItemData | null>(null);
+
+  const handleButtonClick = (name: string) => {
+    setSelectedItem(itemData[name]);
+  };
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    <div>
+      <CheckboxItem name="LED" onButtonClick={handleButtonClick} />
+      <CheckboxItem name="SERVO" onButtonClick={handleButtonClick} />
+      <CheckboxItem name="項目3" onButtonClick={handleButtonClick} />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+      {selectedItem !== null ? (
+        <div>
+          <img src={selectedItem.imagePath} alt={selectedItem.name} />
+          <p> {selectedItem.description} </p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      ) : null}
     </div>
   );
 };
