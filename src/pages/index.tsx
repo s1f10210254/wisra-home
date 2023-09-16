5b1a4502b70de370416a62bc78010efe687dd4d2
s1f10210254 <s1f10210248@iniad.org>
2023-09-16 19:15:08 +0900

    サイドバーにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import { useState } from 'react';
+import styles from './index.module.css';
 
 const Home = () => {
-  interface Props {
-    name: string;
-    onButtonClick: (name: string) => void;
-  }
-
   interface ItemData {
     name: string;
     imagePath: string;
@@ -13,56 +9,51 @@ const Home = () => {
     description: string;
   }
 
-  const itemData: Record<string, ItemData> = {
-    LED: {
-      name: 'LED',
+  const itemData: ItemData[] = [
+    {
+      name: 'INDEX',
       imagePath: 'path_to_your_image/image1.jpeg',
       description: '項目1の説明箇所です',
     },
-    SERVO: {
-      name: '項目2',
+    {
+      name: 'LED',
       imagePath: 'path_to_your_image/image2.jpeg',
       description: '項目2の説明箇所です',
     },
-    項目3: {
-      name: '項目3',
+    {
+      name: 'SERVO',
       imagePath: 'path_to_your_image/image3.jpeg',
       description: '項目3の説明箇所です',
     },
-  };
-
-  const CheckboxItem: React.FC<Props> = ({ name, onButtonClick }) => {
-    const [isChecked, setIschecked] = useState(false);
-    const [count, setCount] = useState(0);
-
-    return (
-      <div>
-        <input type="checkbox" checked={isChecked} onChange={() => setIschecked(!isChecked)} />
-        {name}
-        <input type="number" value={count} onChange={(e) => setCount(Number(e.target.value))} />
-        <button onClick={() => onButtonClick(name)}>ボタン</button>
-      </div>
-    );
-  };
+  ];
 
   const [selectedItem, setSelectedItem] = useState<ItemData | null>(null);
 
-  const handleButtonClick = (name: string) => {
-    setSelectedItem(itemData[name]);
-  };
-
   return (
-    <div>
-      <CheckboxItem name="LED" onButtonClick={handleButtonClick} />
-      <CheckboxItem name="SERVO" onButtonClick={handleButtonClick} />
-      <CheckboxItem name="項目3" onButtonClick={handleButtonClick} />
-
-      {selectedItem !== null ? (
-        <div>
-          <img src={selectedItem.imagePath} alt={selectedItem.name} />
-          <p> {selectedItem.description} </p>
-        </div>
-      ) : null}
+    <div style={{ display: 'flex' }}>
+      <aside className={styles.sidebar}>
+        {itemData.map((item) => (
+          <div
+            key={item.name}
+            className={`sidebarItem ${
+              selectedItem && selectedItem.name === item.name ? 'active' : ''
+            }`}
+            onClick={() => setSelectedItem(item)}
+          >
+            {item.name}
+          </div>
+        ))}
+      </aside>
+      <main className={styles.main}>
+        {selectedItem ? (
+          <div>
+            <img src={selectedItem.imagePath} alt={selectedItem.name} />
+            <p>{selectedItem.description}</p>
+          </div>
+        ) : (
+          <p>項目を選択してください</p>
+        )}
+      </main>
     </div>
   );
 };
